e2e80eb06e83e1e2d7a1c20ac2635214f5370543
marie-bella <71880657+marie-bella@users.noreply.github.com>
2020-12-02 16:20:21 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,43 @@
+class Box{
+    constructor(x,y,width,height){
+        var options ={
+            isStatic:false,
+            restitution:0.4
+
+        };
+
+        this.Visibility=255;
+        this.body= Bodies.rectangle(x,y,width,height,options);
+        World.add(world, this.body);
+        this.width= width;
+        this.height=height;
+
+    }
+
+    display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        if(this.body.speed < 3){
+            push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+           }
+           else{
+            World.remove(world, this.body);
+            push();
+            this.Visiblity = this.Visiblity - 5;
+            pop();
+        
+
+        
+
+          }
+
+}
+
+}
+
+
